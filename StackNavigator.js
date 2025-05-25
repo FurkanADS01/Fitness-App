adc6774f55b6de5a1050dcf272f6d9fc09284b2f
Furkan Polat <furkanpolat2413@gmail.com>
2025-05-25 18:52:13 +0300

    Login Screen
    Giriş yapılacak ekranı hazırladım.

--- StackNavigator.js
+++ StackNavigator.js
@@ -4,12 +4,14 @@ import HomeScreen from './screens/HomeScreen';
 import WorkoutScreen from './screens/WorkoutScreen';
 import FitScreen from './screens/FitScreen';
 import RestScreen from './screens/RestScreen';
+import LoginScreen from './screens/LoginScreen';
 
 const StackNavigator = () => {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen options={{headerShown: false}} name="Login" component={LoginScreen} />
         <Stack.Screen options={{headerShown: false}} name="Home" component={HomeScreen} />
         <Stack.Screen options={{headerShown: false}} name="Workout" component={WorkoutScreen} />
         <Stack.Screen options={{ headerShown: false }} name="Fit" component={FitScreen} />
